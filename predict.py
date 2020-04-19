28b682759a03293bc6b0727dde17a7c10749617a
Ron Barabash <rbarabash@yotpo.com>
2020-04-19 20:15:23 +0300

    feat(Scene-Detector): refactor, using external params and renaming

--- predict.py
+++ predict.py
@@ -1,12 +1,20 @@
+import argparse
+
 import mlflow.keras
 import mlflow.pyfunc
 import pandas as pd
 
 if __name__ == "__main__":
-    mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.set_experiment("vgg16_places")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model-name')
+    parser.add_argument('--model-version')
+    parser.add_argument('--image-url')
+    args = parser.parse_args()
+
+    scenery_model_path = "runs:/{}/{}/".format(args.model_version, args.model_name)
+
+    args = parser.parse_args()
     mlflow.keras.autolog()
-    m = mlflow.pyfunc.load_model(
-        "/Users/rbarabash/Development/yotpo-workspace/scene-detector/VGG16_Places365")
-    model_input = pd.DataFrame(data={'image_url': ['http://places2.csail.mit.edu/imgs/demo/6.jpg']})
+    m = mlflow.pyfunc.load_model(scenery_model_path)
+    model_input = pd.DataFrame(data={'image_url': [args.image_url]})
     m.predict(model_input=model_input)
--- train.py
+++ train.py
@@ -1,15 +1,19 @@
+import argparse
+
 import mlflow.keras
 import mlflow.pyfunc
-
-from scenery_model import SceneryModel
 from keras_models.models.pretrained import vgg16_places365
 
+from scenery_model import SceneryModel
+
 if __name__ == "__main__":
-    mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.set_experiment("vgg16_places")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--artifact-path')
+    args = parser.parse_args()
+
     model = vgg16_places365.VGG16_Places365()
-    model.save(filepath="artifacts/keras_model.h5", overwrite=True)
+    model.save(filepath=args.artifact_path, overwrite=True)
     mlflow.keras.autolog()
 
     sm = SceneryModel()
-    sm.save("artifacts/keras_model.h5")
+    sm.save(args.artifact_path)
