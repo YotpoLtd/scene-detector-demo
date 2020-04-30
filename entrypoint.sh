73b06f8685a66fd47ba28d82bda7ac21a91e6fba
Ron Barabash <rbarabash@yotpo.com>
2020-04-30 19:28:03 +0300

    feat(entrypoint): added graceful shutdown ability

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ export GUNICORN_CMD_ARGS="--statsd-host=${STATSD_HOST} \
                           --log-config ${WORKDIR}/gunicorn_logging.conf \
                           --config ${WORKDIR}/gunicorn_conf.py \
                           --bind ${SERVER_HOST}:${SERVER_PORT} \
-                          --workers ${WORKERS}"
+                          --workers ${WORKERS} \
+                          --graceful-timeout ${GRACEFUL_TIMEOUT_SECONDS}"
 
 #mlflow models serve -m "runs:/$MODEL_VERSION/$MODEL_NAME/" -h $SERVER_HOST -p $SERVER_PORT --no-conda --workers $WORKERS
 export prometheus_multiproc_dir=/tmp
-gunicorn ${GUNICORN_CMD_ARGS} wsgi:app
+exec gunicorn ${GUNICORN_CMD_ARGS} wsgi:app
