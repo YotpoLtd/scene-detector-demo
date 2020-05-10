f69c01289e4776a308530cb1ffb8cedc8a69bba7
Ron Barabash <rbarabash@yotpo.com>
2020-05-10 10:46:47 +0300

    feat(model): no need to copy model code to docker

--- http_predictor.py
+++ http_predictor.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
                              data=model_input,
                              headers={"Content-Type": "application/json; format=pandas-split"})
 
-    print(response)
+    print(response.content)
